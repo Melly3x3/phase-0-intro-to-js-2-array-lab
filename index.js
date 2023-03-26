712afd115faa59b950869b5cf06ce67a42ac34c2
Melvin <jmelvin1134@gmail.com>
2023-03-26 19:13:54 -0400

    array

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const app = "I don't do much."
+
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendcat(Ralph){
+    return cats.push(Milo, Otis, Garfield, Ralph)
+}
+
+function destructivelyPrependCat(Bob){
+    return cats.unshift(Bob, Milo, Otis, Garfield)
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    return cats.pop(Milo, otis)
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    return cats.shift(Otis, Garfield)
+}
+
+function appendcat(broom){
+    return [...kittens, name]
+}
+
+function prependCat(name){
+    return [name, ...kittens]
+}
+
+function removeLastCat(){
+    return cats.slice(0,kittens.length-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
